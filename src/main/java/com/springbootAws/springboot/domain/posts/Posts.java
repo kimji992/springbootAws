d8dbce19ec01f39670071a31d68c22ab3efcdeba
jinil <zzangzc@naver.com>
2022-07-29 13:02:29 +0900

    test

--- src/main/java/com/springbootAws/springboot/domain/posts/Posts.java
+++ src/main/java/com/springbootAws/springboot/domain/posts/Posts.java
@@ -1,11 +1,8 @@
 package com.springbootAws.springboot.domain.posts;
 
-import jdk.internal.vm.annotation.DontInline;
-import jdk.jfr.events.CertificateId;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
 
