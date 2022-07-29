fcd5016bf720d800c50d1ac7d1eb1aaa0e29cc59
jinil <zzangzc@naver.com>
2022-07-29 13:12:34 +0900

    test

--- src/test/java/com/springbootAws/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/springbootAws/springboot/web/PostsApiControllerTest.java
@@ -137,7 +137,7 @@ public class PostsApiControllerTest {
     @Test
     public void BaseTimeEntity_등록() {
         // given
-        LocalDateTime now = LocalDateTime.of(2022,07,22,0,0,0);
+        LocalDateTime now = LocalDateTime.of(2022,07,29,0,0,0);
         postsRepository.save(Posts.builder()
                 .title("title")
                 .content("content")
