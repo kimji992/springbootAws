9a58589787db9654d159814dae767975acc5f8b2
jinil <zzangzc@naver.com>
2022-07-31 16:08:35 +0900

    test

---
+++ src/test/java/com/springbootAws/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,63 @@
+package com.springbootAws.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.env.MockEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile이_조회된다() {
+        // given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_첫_번째가_조회된다() {
+        // given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile이_없으며_default가_조회된다() {
+        // given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+}
